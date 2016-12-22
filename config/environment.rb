8469f3dc225e5462c33d8e9f3666da2441feb56b
Arthur Soiaf <arthursoiaf@gmail.com>
2016-12-22 02:21:07 +0000

    Added mailer settings to env again

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'shielded-anchorage-64464.herokuapp.com',
+  :domain         => 'https://shielded-anchorage-64464.herokuapp.com/',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
